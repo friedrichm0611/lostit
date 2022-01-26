a330256bd474902028bac09ad748ac9881b40a5b
Matthias Friedrich <matthias.friedrich@de.experis.com>
2022-01-26 14:59:00 +0100

    Inititial Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter,
+  Route
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lost in Translation App</h1>
+      <h4>
+        <span class="material-icons">language</span>
+      </h4>
     </div>
   );
 }
